87adf7c64127083c7a1195a07014264cdbda21a7
Tony Flury <anthony.flury@btinternet.com>
2020-11-22 13:45:47 +0000

    final.py
    Smart formatting

--- final.py
+++ final.py
@@ -1,6 +1,14 @@
 class Temperature:
     _units = {'C':'celsius','F':'fahrenheit'}
     def __init__(self, initial):
+        """Temperature in C or F with automatic translation between the two
+        
+             initial : numeric or str. If numeric is assumed to be initial value in degrees C.
+                                       If str is assumed to be formatted with last letter C or F:
+                                       e.g. '212F' will be taken to be 212 degrees Farenheit. 
+             raises ValueError if str is not formatted correctly (eg. no unit as last letter),
+                               or numeric is not valid converted to a float.
+        """
         attribute = 'celsius'
         try:
             attribute = Temperature._units[initial[-1].upper()]
@@ -14,8 +22,7 @@ class Temperature:
             setattr(self, attribute, float(value))
         except (ValueError, TypeError):
                 raise ValueError(f'Invalid initial Temperature given: {initial}') from None
-
-            
+  
     @property
     def celsius(self):
         return self._celsius
@@ -30,6 +37,10 @@ class Temperature:
         self._celsius = 5*(value-32)/9
         
     def __format__(self, format_spec):
+        """Addition to the format spec:
+           if the Type Character is F or C then format the Fahrenheit or Celsius respectively
+           The rest of the format spec is honoured by default
+        """
         attribute = Temperature._units.get(format_spec[-1],None)
         if not attribute:
             return format(self.celsius,format_spec)
